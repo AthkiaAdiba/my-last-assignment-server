d53f8e9da2003f670d9a5f77c5793226482a92b2
AthkiaAdiba <athkiaadiba@gmail.com>
2024-06-01 23:51:34 +0600

    added code of the saving user info to the database

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ui1n29x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,7 +31,12 @@ async function run() {
         const userCollection = database.collection("users");
 
 
-
+        // users related api
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
