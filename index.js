a613d4d5ea1777a13559936440474e4145917e5a
AthkiaAdiba <athkiaadiba@gmail.com>
2024-06-02 08:10:12 +0600

    created jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -29,6 +30,14 @@ async function run() {
 
         const database = client.db("petsDB");
         const userCollection = database.collection("users");
+
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token });
+        })
 
 
         // users related api
